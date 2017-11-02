5627fe5b5ab9da4b7391b5d55587c1d062e25508
sh3lep <sh3lep@gmail.com>
2017-11-02 23:59:06 +0300

    Исправлен lesson3, кроме squareBetweenExists; сделаны первые несколько заданий из lesson4.

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -112,7 +112,8 @@ fun lcm(m: Int, n: Int): Int {
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
 fun minDivisor(n: Int): Int {
-    if (isPrime(n)) return n
+    if (isPrime(n))
+        return n
     for (i in 2..(n / 2)) {
         if (n % i == 0)
             return i
